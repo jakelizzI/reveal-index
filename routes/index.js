1f343e76f19fb7692e5dd469d5f4759547b9ab60
MEDIASEEK\ginga.nishizawa <ginga.nishizawa@mediaseek.co.jp>
2017-10-07 11:14:15 +0900

    node express と reveal の簡易コミット（今後大幅な修正あり）

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+    fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  fs.readdir('./md/', function(err, files){
+    if(err) throw err;
+    res.render('index', { title: 'Express', fileList: files});
+  });
+});
+
+module.exports = router;
